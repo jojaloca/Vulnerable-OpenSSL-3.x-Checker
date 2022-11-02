422030c3d9e4c4334eff8da4095a8a3a34edad3b
jojaloca <jojaloca@gmail.com>
2022-11-02 13:41:44 +0100

    Update openssl3-check.sh

--- openssl3-check.sh
+++ openssl3-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # if error stop script
-set -e
+#set -e
 
 # binary full paths
 AWK=$(which awk)
@@ -16,6 +16,10 @@ SUDO=$(which sudo)
 ECHO=$(which echo)
 PS=$(which ps)
 WC=$(which wc)
+STRINGS=$(which strings)
+FIND=$(which find)
+
+REGEX='^OpenSSL 3.0.[0-6]'
 
 # check root privs
 if [[ $($ID -u) -ne 0 ]]; then
@@ -35,21 +39,31 @@ else
 fi
 
 # check openssl version
-$ECHO "[info] Checking OpenSSL version installed"
+$ECHO "[info] Checking OpenSSL version installed (SO)"
 if [ -f "$OPENSSL" ]; then
   openssl_version=$($OPENSSL version);
-  if [[ $($ECHO -n $openssl_version | $GREP -i -E "^OpenSSL 3.0.[0-6]") != '' ]]; then
-    $ECHO "[critical] Vulnerable version found: ${openssl_version}";
+  if [[ $($ECHO -n $openssl_version | $GREP -i -E "$REGEX") != '' ]]; then
+    $ECHO "[critical] Vulnerable version found in OS: ${openssl_version}";
   else
-    $ECHO "[info] Secure version installed: ${openssl_version}";
+    $ECHO "[info] Secure version installed in OS: ${openssl_version}";
   fi
 else
-  $ECHO "[info] OpenSSL not found";
+  $ECHO "[info] OpenSSL not found in OS";
+fi
+
+# search openssl libraries in system
+$ECHO "[info] Searching vulnerable OpenSSL library (System)"
+libraries_found=$($FIND / -type f '(' -name "libcrypto3.so" -o -name "libcrypto.so.3" -o -name "libssl3.so" -o -name "libssl.so.3" -o -name "libssl3.a" -o -name "libssl.a.3" -o -name "libcrypto3.a" -o -name "libcrypto.a.3" ')' 2>/dev/null)
+if [[ $($ECHO "$libraries_found" | $WC -w) ]]; then
+  $ECHO "[critical] OpenSSL vulnerable library found in system";
+  for file in $libraries_found; do
+    $ECHO $file;
+  done;
 fi
 
 # check processes with vulnerable OpenSSL dynamic library loaded
 $ECHO "[info] Checking system processes with vulnerable OpenSSL library loaded (dynamic linked)";
-vulnerable_processes=$($SUDO $LSOF -n 2>/dev/null | $GREP -E 'libssl3.so|libcrypto.so.3' | $AWK '{print $2}' | $SHORT -n | $UNIQ);
+vulnerable_processes=$($SUDO $LSOF -n 2>/dev/null | $GREP -E 'libssl3.so|libssl.so.3|libcrypto3.so|libcrypto.so.3' | $AWK '{print $2}' | $SHORT -n | $UNIQ);
 if [[ $($ECHO -n vulnerable_processes | $WC -l) -ge 0 ]]; then
   $ECHO "[critical] Processes using vulnerable OpenSSL version (dynamic library loaded)";
   $PS -o '%p %a' $vulnerable_processes;
